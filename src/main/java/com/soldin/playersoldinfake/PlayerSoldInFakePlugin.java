30e4d22ad723b5b0e9ec4fef5b6263a3241be415
Soldn <kyryll524@gmail.com>
2025-08-24 21:36:06 +0100

    Update PlayerSoldInFakePlugin.java

--- src/main/java/com/soldin/playersoldinfake/PlayerSoldInFakePlugin.java
+++ src/main/java/com/soldin/playersoldinfake/PlayerSoldInFakePlugin.java
@@ -2,23 +2,17 @@ package com.soldin.playersoldinfake;
 
 import org.bukkit.plugin.java.JavaPlugin;
 
-public class PlayerSoldInFakePlugin extends JavaPlugin {
-
-    private static PlayerSoldInFakePlugin instance;
+public final class PlayerSoldInFakePlugin extends JavaPlugin {
 
     @Override
     public void onEnable() {
-        instance = this;
-        saveDefaultConfig(); // создаём config.yml если его нет
-        getLogger().info("PlayerSoldInFake включён! Автор: Soldi_n");
+        // Загружаем конфиг
+        saveDefaultConfig();
+        getLogger().info("PlayerSoldInFake включён!");
     }
 
     @Override
     public void onDisable() {
         getLogger().info("PlayerSoldInFake выключен!");
-    }
-
-    public static PlayerSoldInFakePlugin getInstance() {
-        return instance;
     }
 }
